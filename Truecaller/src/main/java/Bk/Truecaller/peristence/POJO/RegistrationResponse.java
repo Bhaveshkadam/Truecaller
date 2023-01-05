5c3907e7ba2831ac3ebc550cbb3713dd3c59b5dc
Bhaveshkadam <bhaukadam508@gmail.com>
2023-01-05 20:58:37 +0200

    added code

---
+++ Truecaller/src/main/java/Bk/Truecaller/peristence/POJO/RegistrationResponse.java
@@ -0,0 +1,22 @@
+package Bk.Truecaller.peristence.POJO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationResponse{
+
+
+  
+    private String message;
+
+}
