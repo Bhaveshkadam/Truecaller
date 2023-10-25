c5da09c2b78cee430170f37fb8a9e477799625e2
Bhaveshkadam <bhaukadam508@gmail.com>
2023-10-25 19:25:01 +0530

    Added userController and service

---
+++ Truecaller/src/main/java/Bk/Truecaller/controller/UserController.java
@@ -0,0 +1,10 @@
+package Bk.Truecaller.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+public class UserController {
+
+}
---
+++ Truecaller/src/main/java/Bk/Truecaller/service/UserService.java
@@ -0,0 +1,7 @@
+package Bk.Truecaller.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
