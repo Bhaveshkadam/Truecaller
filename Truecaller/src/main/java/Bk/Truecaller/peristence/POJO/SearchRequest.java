889603785066231214aaccbc59c5a7147b7fe4f8
Bhaveshkadam <bhaukadam508@gmail.com>
2023-10-25 19:09:46 +0530

    Add and modify SearchRequest/Response

--- Truecaller/src/main/java/Bk/Truecaller/peristence/POJO/SearchRequest.java
+++ Truecaller/src/main/java/Bk/Truecaller/peristence/POJO/SearchRequest.java
@@ -1,4 +1,22 @@
 package Bk.Truecaller.peristence.POJO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SearchRequest {
+
+    @NotBlank
+    @Pattern(regexp ="/^([+]\\d{2}[ ])?\\d{10}$/", message = "enter valid number")
+    private BigInteger number;
 }
--- Truecaller/src/main/java/Bk/Truecaller/peristence/POJO/SearchResponse.java
+++ Truecaller/src/main/java/Bk/Truecaller/peristence/POJO/SearchResponse.java
@@ -1,4 +1,17 @@
 package Bk.Truecaller.peristence.POJO;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SearchResponse {
+
+    @NotBlank
+    private String message;
 }
